07636bcdff88663bd87993c2e70f711015f956b3
Vladimir <vladimircuriel@outlook.com>
2024-05-11 22:01:57 -0400

    feat: Update Banner component text for live demo
    This commit updates the text in the Banner component to clarify that the web is a live demo and not intended for actual use. This change improves the clarity and user understanding of the purpose of the web page.

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,10 +4,10 @@ const Banner = () => {
       <div className="flex items-center justify-center size-full">
         <p className="text-center text-pretty">
           <span className="hidden sm:inline-flex animate-background-shine bg-[linear-gradient(110deg,#144EE3,45%,#1e293b,55%,#EB568E)] bg-[length:250%_100%] bg-clip-text text-xl text-transparent">
-            {"This web it's not intended to be used. It's functional though."}
+            {"  This web is a live demo. It is not intended to be used."}
           </span>
           <span className="inline-flex sm:hidden animate-background-shine bg-[linear-gradient(110deg,#144EE3,45%,#1e293b,55%,#EB568E)] bg-[length:250%_100%] bg-clip-text text-xl text-transparent">
-            This web it is not intended to be used.
+            This web is a live demo.
           </span>
         </p>
       </div>
