998b6a1bbdfba66340a114e7b54db0ff7c250f0f
Vladimir <vladimircuriel@outlook.com>
2024-05-11 21:04:13 -0400

    feat: Update UserProfile page to redirect based on user's username
    This commit updates the UserProfile page to redirect the user based on their username. If the user has a username, they will be redirected to the corresponding user profile page. This enhancement improves the user experience by providing personalized and relevant content.

--- src/app/(dashboard)/dashboard/user/profile/page.tsx
+++ src/app/(dashboard)/dashboard/user/profile/page.tsx
@@ -1,14 +1,19 @@
+import { User as AuthUser } from "@auth/core/types";
 import { auth } from "@lib/auth/auth";
 import { redirect } from "next/navigation";
+
+interface User extends AuthUser {
+  username?: string;
+}
 
 const UserProfile = async () => {
   const session = await auth();
   if (!session) return redirect("/login");
 
-  const user = session.user;
+  const user: User | undefined = session.user;
   if (!user) return redirect("/login");
 
-  redirect(`/dashboard/users/${user.username}`);
+  if (user.username) redirect(`/dashboard/users/${user.username}`);
 
   return <main>UserProfile</main>;
 };
